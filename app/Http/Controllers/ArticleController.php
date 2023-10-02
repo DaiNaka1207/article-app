3889f2f2e0472d4f26e761143a9b31dc4c8236ff
DaiNaka <wxfpk432@gmail.com>
2023-10-03 01:32:47 +0900

    :sparkles: Create getting articles controller

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class ArticleController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        // 入力ボックスに入力された値が存在するかを判定
+        if (isset($request->username)) {
+
+            // 入力ボックスに入力がある場合の処理
+            // Zennで記事一覧を取得して$resp変数へ代入
+            $resp = Http::get('https://zenn.dev/api/articles?username='.$request->username.'&order=latest');
+
+            // 取得した記事をJson形式に変換して$resp変数に代入
+            $resp = $resp->json();
+
+            // 取得した記事の'article'オブジェクトを$articles変数に代入
+            $articles = $resp['articles'];
+            // dd($articles);
+
+            // articles変数をarticlesという名前でtopビューへ渡して表示
+            return view('top', ['articles' => $articles]);
+
+        } else {
+
+            // 入力ボックスに入力がない場合の処理
+            // topビューを表示
+            return view('top');
+
+        };
+    }
+}
